0cc469f2f175d9fb50b6b9bc42c39919c64a5723
Marcus Phan <58803185+MarcusPhan@users.noreply.github.com>
2026-06-06 12:57:14 +1000

    feat: sentry

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "bistro-ai",
+  project: "javascript-nextjs",
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+});
--- src/app/(canvas)/canvas/page.tsx
+++ src/app/(canvas)/canvas/page.tsx
@@ -2,6 +2,7 @@
 
 import { useOnboardingStore } from "@/store/onboardingStore";
 import CanvasCard from "@/components/canvas/CanvasCard";
+import * as Sentry from "@sentry/nextjs";
 
 const CARDS = [
   {
@@ -34,6 +35,27 @@ export default function CanvasPage() {
           <CanvasCard key={c.title} {...c} />
         ))}
       </div>
+
+      {/* How to test Sentry error capturing   */}
+      {/* <button
+        type="button"
+        onClick={() => {
+          throw new Error("Sentry Test Error");
+        }}
+      >
+        Break the world
+      </button> */}
+      
+      {/* How to Log with Sentry */}
+      {/* <button
+        type="button"
+        onClick={() => {
+          Sentry.logger.info("User clicked Test Sentry Log button", { log_source: 'sentry_test' });
+        }}
+        className="bg-red-50"
+      >
+        Test Sentry Log
+      </button> */}
     </div>
   );
 }
